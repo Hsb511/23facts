331b18dfc6720716630ce745aaa82129bb82b939
Hsb511 <hugo-selle@hotmail.fr>
2022-09-14 22:01:05 +0200

    [ADD] AppVersions for 0.0.1

---
+++ buildSrc/src/main/java/AppVersions.kt
@@ -0,0 +1,8 @@
+object AppVersions {
+    private const val prodVersion = 0
+    private const val stagingVersion = 0
+    private const val debugVersion = 1
+
+    const val versionName = "$prodVersion.$stagingVersion.$debugVersion"
+    const val versionCode = prodVersion * 1_000_000 + stagingVersion * 1_000 + debugVersion
+}
