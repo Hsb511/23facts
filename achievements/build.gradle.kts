02514d6d11d6dfab83d1ecaf2d11e1412f3e82e2
Hsb511 <hugo-selle@hotmail.fr>
2022-09-16 17:46:19 +0200

    [ADD] Creating the :achievements module

---
+++ achievements/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.COMPOSE
+    }
+}
+
+dependencies {
+    // Compose
+    implementation("androidx.compose.compiler:compiler:${Versions.COMPOSE}")
+    implementation("androidx.compose.runtime:runtime-livedata:${Versions.COMPOSE}")
+    implementation("androidx.navigation:navigation-compose:2.5.1")
+    implementation("androidx.compose.material:material:${Versions.COMPOSE}")
+    implementation("androidx.compose.ui:ui:${Versions.COMPOSE}")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
+    debugImplementation("androidx.customview:customview:1.2.0-alpha01")
+    debugImplementation("androidx.customview:customview-poolingcontainer:1.0.0")
+
+    implementation("androidx.core:core-ktx:${Versions.ANDROID_CORE}")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
