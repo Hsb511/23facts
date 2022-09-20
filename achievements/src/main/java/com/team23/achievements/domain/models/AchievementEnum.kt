5d4386b0ce4d0b44e3c0544e37930540df4af28c
Hsb511 <hugo-selle@hotmail.fr>
2022-09-20 13:19:49 +0200

    [ADD] Adding a new achievement and facts to the db

--- achievements/src/main/java/com/team23/achievements/domain/models/AchievementEnum.kt
+++ achievements/src/main/java/com/team23/achievements/domain/models/AchievementEnum.kt
@@ -3,5 +3,6 @@ package com.team23.achievements.domain.models
 import com.team23.achievements.R
 
 enum class AchievementEnum(val nameResId: Int, val imageResId: Int, val popupMessageResId: Int) {
-    APP_ICON_CLICKED_23_TIMES(R.string.achievements_iconophile, R.drawable.iconophile, R.string.achievements_iconophile_message)
+    APP_ICON_CLICKED_23_TIMES(R.string.achievements_iconophile, R.drawable.iconophile, R.string.achievements_iconophile_message),
+    HOME_ICON_CLICKED_23_SECONDS(R.string.achievements_homebody, R.drawable.homebody, R.string.achievements_homebody_message)
 }
