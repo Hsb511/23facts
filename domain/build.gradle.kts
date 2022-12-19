73ca7c0fac0e91405271f618d03275c31b919f90
Hsb511 <hugo-selle@hotmail.fr>
2022-12-19 23:49:40 +0100

    [TEST] Unlock1IconophileUseCase

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -4,14 +4,15 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
-    // Hilt
     implementation("javax.inject:javax.inject:1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk:1.13.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
---
+++ domain/src/test/java/com/team23/domain/achievements/Unlock1IconophileUseCaseTest.kt
@@ -0,0 +1,78 @@
+package com.team23.domain.achievements
+
+import com.team23.achievements.usecases.Unlock1IconophileUseCase
+import com.team23.achievements.usecases.UnlockAchievementUseCase
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class Unlock1IconophileUseCaseTest {
+    companion object {
+        private const val ACHIEVEMENT_1_NAME = "ACHIEVEMENT_1_NAME"
+    }
+
+    private val unlockAchievementUseCase = mockk<UnlockAchievementUseCase> {
+        coEvery { this@mockk.invoke(any()) } returns Unit
+    }
+    private val unlock1IconophileUseCase = Unlock1IconophileUseCase(unlockAchievementUseCase)
+
+    @Test
+    fun `Given app icon 0 time clicked, When unlock1IconophileUseCase is invoked, Then returns null`() =
+        runBlocking {
+            // Given
+            val appIconClickAmount = 0
+
+            // When
+            val result = unlock1IconophileUseCase.invoke(appIconClickAmount, ACHIEVEMENT_1_NAME)
+
+            // Then
+            assertEquals(null, result)
+            coVerify(exactly = 0) { unlockAchievementUseCase(any()) }
+        }
+
+    @Test
+    fun `Given app icon 1 time clicked, When unlock1IconophileUseCase is invoked, Then returns null`() =
+        runBlocking {
+            // Given
+            val appIconClickAmount = 1
+
+            // When
+            val result = unlock1IconophileUseCase.invoke(appIconClickAmount, ACHIEVEMENT_1_NAME)
+
+            // Then
+            assertEquals(null, result)
+            coVerify(exactly = 0) { unlockAchievementUseCase(any()) }
+        }
+
+    @Test
+    fun `Given app icon 22 time clicked, When unlock1IconophileUseCase is invoked, Then returns null`() =
+        runBlocking {
+            // Given
+            val appIconClickAmount = 22
+
+            // When
+            val result = unlock1IconophileUseCase.invoke(appIconClickAmount, ACHIEVEMENT_1_NAME)
+
+            // Then
+            assertEquals(null, result)
+            coVerify(exactly = 0) { unlockAchievementUseCase(any()) }
+        }
+
+    @Test
+    fun `Given app icon 23 times clicked, When unlock1IconophileUseCase is invoked, Then returns null`() =
+        runBlocking {
+            // Given
+            val appIconClickAmount = 23
+
+            // When
+            val result = unlock1IconophileUseCase.invoke(appIconClickAmount, ACHIEVEMENT_1_NAME)
+
+            // Then
+            assertEquals(ACHIEVEMENT_1_NAME, result!!.name)
+            assertEquals(true, result.isFound)
+            coVerify(exactly = 1) { unlockAchievementUseCase(ACHIEVEMENT_1_NAME) }
+        }
+}
