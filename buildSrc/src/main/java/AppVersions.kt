740be78b4b7c74589907103ac8a22c3897ea5a11
Hsb511 <hugo-selle@hotmail.fr>
2023-01-21 14:21:49 +0100

    [VER] Updating Version to 1.0.0

--- buildSrc/src/main/java/AppVersions.kt
+++ buildSrc/src/main/java/AppVersions.kt
@@ -1,7 +1,7 @@
 object AppVersions {
-    private const val prodVersion = 0
+    private const val prodVersion = 1
     private const val stagingVersion = 0
-    private const val debugVersion = 3
+    private const val debugVersion = 0
 
     const val versionName = "$prodVersion.$stagingVersion.$debugVersion"
     const val versionCode = prodVersion * 1_000_000 + stagingVersion * 1_000 + debugVersion
